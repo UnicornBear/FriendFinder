be209534f9a0e89b9d4fa5dc63f990a4e11b1667
UnicornBear <jtbregenzer@gmail.com>
2019-06-14 10:18:49 -0600

    setting up structure and starting key files

---
+++ app/data/friends.js
@@ -0,0 +1,97 @@
+// arrays that hold all the friends that have filled out the finder survey
+
+// start with dummy data
+
+var friendList = [{
+    name: 'Rich',
+    profilePic: '',
+    scores: [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+},{
+    name: 'Logan',
+    profilePic: '',
+    scores: [
+        4,
+        5,
+        5,
+        2,
+        4,
+        5,
+        4,
+        4,
+        3,
+        4
+      ]
+},{
+    name: 'Stephanie',
+    profilePic: '',
+    scores: [
+        4,
+        5,
+        5,
+        2,
+        4,
+        5,
+        4,
+        4,
+        3,
+        4
+      ]
+},{
+    name: 'Julie',
+    profilePic: '',
+    scores: [
+        4,
+        5,
+        5,
+        2,
+        4,
+        5,
+        4,
+        4,
+        3,
+        4
+      ]
+},{
+    name: 'Jake',
+    profilePic: '',
+    scores: [
+        4,
+        5,
+        5,
+        2,
+        4,
+        5,
+        4,
+        4,
+        3,
+        4
+      ]
+},{
+    name: 'Rachel',
+    profilePic: '',
+    scores: [
+        4,
+        5,
+        5,
+        2,
+        4,
+        5,
+        4,
+        4,
+        3,
+        4
+      ]
+},
+
+];
---
+++ server.js
@@ -0,0 +1,8 @@
+// requires
+var express = require('express');
+var path = require('path');
+
+// express app and PORT variable
+var app = express();
+var PORT = process.env.PORT || 8800;
+
