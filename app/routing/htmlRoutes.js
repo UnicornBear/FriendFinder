e1723d2da02e65c0347fe111e0e43ae80b6714d9
UnicornBear <jtbregenzer@gmail.com>
2019-06-15 15:19:38 -0600

    update module function to include inside {} both survey and html

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,12 +3,15 @@ var path = require('path');
 
 // routing to the survey
 module.exports = function(app){
+    
     app.get('/survey',function(req,res) {
         res.sendFile(path.join(_dirname + '/../public/survey.html'));
     });
-};
 
-// routing to the home page
-app.use(function(req,res) {
-        res.sendFile(path.join(_dirname + '/../public/home.html'));
+
+    // routing to the home page
+    app.use(function(req,res) {
+            res.sendFile(path.join(_dirname + '/../public/home.html'));
     });
+
+};
