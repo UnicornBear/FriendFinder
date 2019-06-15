e5af9cd64b6c939d455bed925d26caafc29d8f60
UnicornBear <jtbregenzer@gmail.com>
2019-06-15 08:26:16 -0600

    set up routing to survey and home .html

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,14 @@
+// require path
+var path = require('path');
+
+// routing to the survey
+module.exports = function(app){
+    app.get('/survey',function(req,res) {
+        res.sendFile(path.join(_dirname + '/../public/survey.html'));
+    });
+};
+
+// routing to the home page
+app.use(function(req,res) {
+        res.sendFile(path.join(_dirname + '/../public/home.html'));
+    });
