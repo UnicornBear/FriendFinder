eafb06bf36d67465e12d37f48bf905abd193c2aa
UnicornBear <jtbregenzer@gmail.com>
2019-06-15 21:55:17 -0600

    change _dirname to __dirname

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ var app = express();
 var PORT = process.env.PORT || 8800;
 
 // set up express to parse the data
-
-app.use(express.static(_dirname + "app/public"));
+app.use(express.static(__dirname + "app/public"));
 app.use(bodyParser.json());
 app.use(bodyParser.text());
 app.use(bodyParser.urlencoded({
@@ -21,9 +20,9 @@ app.use(bodyParser.json({
 
 
 // require the routing to api and home html
-
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
+
 
 // need to start the server so it can listen to command
 app.listen(PORT, function() {
